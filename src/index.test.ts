1df77ecb3807fb76f26dfa003dc9a3600925ea0a
katryo <katoryo55@gmail.com>
2020-05-29 11:49:18 -0400

    Start developing parser

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,5 +1,29 @@
-import { testText } from './index';
+import { parse } from './index';
 
 test('adds 1 + 2 to equal 3', () => {
-  expect(testText).toBe('abc');
+  const testText = 'abc [music dance] abc';
+  const result = parse(testText);
+
+  expect(result).toBe({
+    sections: [
+      {
+        paragraphs: ['abc'],
+        music: '',
+        sound: '',
+        filter: '',
+        bg: '',
+        image: '',
+        key: '0',
+      },
+      {
+        paragraphs: ['def'],
+        music: 'dance',
+        sound: '',
+        filter: '',
+        bg: '',
+        image: '',
+        key: '1',
+      },
+    ],
+  });
 });
--- src/index.ts
+++ src/index.ts
@@ -13,22 +13,26 @@ const inputPath = path.join(process.cwd(), 'input', 'episode.txt');
 const text = fs.readFileSync(inputPath, 'utf8');
 // const paragraphs = text.split('\n')
 
-// const outputJSON = {
-//   episode: {
-//     series_title: 'Zombie Story',
-//     episode_title: 'Episode 1',
-//     creator: 'katryo',
-//     sections: [
-//       { paragraphs: paragraphs, bgm: 'lets-dance', se: '', filter: 'black', bg: 'building', image: '', key: '1' },
-//     ],
-//     audio_map: { 'lets-dance': 'https://katryomusic.s3.amazonaws.com/lets_dance.mp3' },
-//     image_map: { building: 'https://denkinovel-dev-test.s3.amazonaws.com/nyc-building.jpg' },
-//     color_map: { black: '#000000', white: '#ffffff' },
-//   },
-// }
 // const outputPath = path.join(process.cwd(), 'output', 'episode.json')
 
 // fs.writeFileSync(outputPath, JSON.stringify(outputJSON), 'utf8')
+
+const _exampleOutput = {
+  episode: {
+    series_title: 'Zombie Story',
+    episode_title: 'Episode 1',
+    creator: 'katryo',
+    sections: [
+      { paragraphs: ['abc'], music: 'lets-dance', sound: '', filter: 'black', bg: 'building', image: '', key: '1' },
+    ],
+    audio_map: { 'lets-dance': 'https://katryomusic.s3.amazonaws.com/lets_dance.mp3' },
+    image_map: { building: 'https://denkinovel-dev-test.s3.amazonaws.com/nyc-building.jpg' },
+    color_map: { black: '#000000', white: '#ffffff' },
+    default_bg: 'black',
+    default_filter: 'black',
+    default_text_color: 'white',
+  },
+};
 
 const DEFAULT_BG = 'black';
 const DEFAULT_MUSIC = '';
@@ -42,33 +46,62 @@ const ERROR_OBJ = {
   nextState: {},
 };
 
-let chars = [];
-let paragraphs = [];
+interface State {
+  [key: string]: {
+    func: Function;
+    nextState: State;
+  };
+}
 
-const initState = {
-  '[': { func: () => {}, nextState: {} },
+const initState: State = {
+  '[': { func: () => {}, nextState: {} }, // To be replaced with inBracketState
   ']': ERROR_OBJ,
   '\n': { func: () => {}, nextState: {} },
-  OTHERS: { func: () => {}, nextState: {} },
+  OTHERS: { func: () => {}, nextState: {} }, // To be replaced
 };
 
 initState.OTHERS.nextState = initState;
 
-const inBracketState = {
+const inBracketState: State = {
   ']': { func: () => {}, nextState: initState },
-  ' ': {},
-  '\n': { func: () => {}, nextState: {} },
+  ' ': { func: () => {}, nextState: initState }, // To be replaced
+  '\n': { func: () => {}, nextState: initState }, // To be replaced
   '[': ERROR_OBJ,
-  OTHERS: {},
+  OTHERS: { func: () => {}, nextState: initState }, // To be replaced
 };
 
 initState['['].nextState = inBracketState;
 inBracketState['\n'].nextState = inBracketState;
 
-let currentMusic = DEFAULT_MUSIC;
-let currentSound = DEFAULT_SOUND;
-let currentBG = DEFAULT_BG;
+const parse = (text: string) => {
+  let cur = {
+    key: 0,
+    music: DEFAULT_MUSIC,
+    sound: DEFAULT_SOUND,
+    bg: DEFAULT_BG,
+    paragraphs: [],
+    chars: [],
+  };
 
-const testText = 'abc [music dance] abc';
+  let i = 0;
+  let state = initState;
 
-export { testText };
+  for (let i = 0; i < text.length; i++) {
+    const char = text[i];
+    if (char in state) {
+      const { func, nextState } = state[char];
+      const actionFunc = func(char); // TODO: think
+      cur = actionFunc(cur);
+      state = nextState;
+    }
+  }
+
+  const outputJSON = {
+    sections: [
+      { paragraphs: ['abc'], music: 'lets-dance', sound: '', filter: 'black', bg: 'building', image: '', key: '1' },
+    ],
+  };
+  return outputJSON;
+};
+
+export { parse };
